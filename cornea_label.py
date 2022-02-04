6aefa90f63b6aac784f3da93fb41dfdc486b5a0d
Liu <liu473@iu.edu>
2022-02-04 18:34:11 -0500

    able to draw 3 layers;verify that masks match the orgianl image

--- cornea_label.py
+++ cornea_label.py
@@ -28,19 +28,19 @@ def begueradj_draw(event,former_x,former_y,flags,param):
     elif event == cv2.EVENT_MOUSEMOVE:
         if drawing==True:
             if mode==True and layer_num == 1:
-                cv2.line(new_img,(current_former_x,current_former_y),(former_x,former_y),(0,0,255),5)
+                cv2.line(new_img,(current_former_x,current_former_y),(former_x,former_y),(0,0,255),8)
                 current_former_x = former_x
                 current_former_y = former_y
                 layer_1_x.append(current_former_x)
                 layer_1_y.append(current_former_y)
             elif mode == True and layer_num == 2:
-                cv2.line(new_img,(current_former_x,current_former_y),(former_x,former_y),(0,255,0),5)
+                cv2.line(new_img,(current_former_x,current_former_y),(former_x,former_y),(0,255,0),8)
                 current_former_x = former_x
                 current_former_y = former_y
                 layer_2_x.append(current_former_x)
                 layer_2_y.append(current_former_y)
             elif mode == True and layer_num == 3:
-                cv2.line(new_img,(current_former_x,current_former_y),(former_x,former_y),(255,0,0),5)
+                cv2.line(new_img,(current_former_x,current_former_y),(former_x,former_y),(255,0,0),8)
                 current_former_x = former_x
                 current_former_y = former_y
                 layer_3_x.append(current_former_x)
@@ -51,19 +51,19 @@ def begueradj_draw(event,former_x,former_y,flags,param):
     elif event==cv2.EVENT_LBUTTONUP:
         drawing=False
         if mode==True and layer_num == 1:
-            cv2.line(new_img,(current_former_x,current_former_y),(former_x,former_y),(0,0,255),5)
+            cv2.line(new_img,(current_former_x,current_former_y),(former_x,former_y),(0,0,255),8)
             current_former_x = former_x
             current_former_y = former_y
             layer_1_x.append(current_former_x)
             layer_1_y.append(current_former_y)
         elif mode == True and layer_num == 2:
-            cv2.line(new_img,(current_former_x,current_former_y),(former_x,former_y),(0,255,0),5)
+            cv2.line(new_img,(current_former_x,current_former_y),(former_x,former_y),(0,255,0),8)
             current_former_x = former_x
             current_former_y = former_y
             layer_2_x.append(current_former_x)
             layer_2_y.append(current_former_y)
         elif mode == True and layer_num == 3:
-            cv2.line(new_img,(current_former_x,current_former_y),(former_x,former_y),(255,0,0),5)
+            cv2.line(new_img,(current_former_x,current_former_y),(former_x,former_y),(255,0,0),8)
             current_former_x = former_x
             current_former_y = former_y
             layer_3_x.append(current_former_x)
@@ -72,7 +72,7 @@ def begueradj_draw(event,former_x,former_y,flags,param):
     return former_x,former_y
 
 
-dir_name = '/Users/mac/Desktop/lab/pupil_1/original_image/'
+dir_name = 'G:/OCM_MCFM_Data/E/Combined_data/Tissue imaging/PiG imaging/Eye1/endo1/OCM_1mm by 1mm/asc_file/'
 
 file_name = os.listdir(dir_name)[1]
 img_array = np.loadtxt(dir_name + file_name)
@@ -80,6 +80,7 @@ img_array1 = (img_array/np.max(img_array)*200).astype(np.uint8)
 new_img = cv2.cvtColor(img_array1.astype(np.uint8), cv2.COLOR_GRAY2BGR)
 new_img = cv2.rotate(new_img, cv2.ROTATE_90_COUNTERCLOCKWISE)
 input_img = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)#for combining mask image later
+reserve_img = new_img.copy()
 
 layer_num = 1
 cv2.namedWindow("cornea")
@@ -88,6 +89,14 @@ while(1):
     cv2.imshow("cornea", new_img)
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
+        layer_1_x = []
+        layer_1_y = []
+
+        layer_2_x = []
+        layer_2_y = []
+
+        layer_3_x = []
+        layer_3_y = []
         cv2.destroyAllWindows()
         break
     elif k == ord('t'):
@@ -98,11 +107,35 @@ while(1):
         layer_num = 3
     elif k == ord('d'):
         mask_img = np.zeros((new_img.shape[0], new_img.shape[1]), np.uint8)
-        for i in range(0, len(layer_1_x) - 1):
-            cv2.line(mask_img,(layer_1_x[i],layer_1_y[i]),(layer_1_x[i + 1],layer_1_y[i + 1]), 1, 5)
-        add_image = cv2.addWeighted(input_img, 0.7, mask_img, 3, 0)
+        if len(layer_1_x) >=2:
+            for i in range(0, len(layer_1_x) - 1):
+                cv2.line(mask_img,(layer_1_x[i],layer_1_y[i]),(layer_1_x[i + 1],layer_1_y[i + 1]), 1, 8)
+        if len(layer_2_x) >=2:
+            for i in range(0, len(layer_2_x) - 1):
+                cv2.line(mask_img,(layer_2_x[i],layer_2_y[i]),(layer_2_x[i + 1],layer_2_y[i + 1]), 1, 8)
+        if len(layer_3_x) >=2:
+            for i in range(0, len(layer_3_x) - 1):
+                cv2.line(mask_img,(layer_3_x[i],layer_3_y[i]),(layer_3_x[i + 1],layer_3_y[i + 1]), 1, 8)
+        #tf_input = tf.keras.utils.array_to_img(input_img)
+        #tf_mask = tf.keras.utils.array_to_img(np.repeat(np.expand_dims(mask_img,-1),3,-1))
+        #plt.imshow(tf.keras.utils.array_to_img(input_img), cmap = "gray")
+        add_image = cv2.addWeighted(input_img, 0.7, mask_img, 0.3, 0)
         cv2.imshow('combine', add_image)
+        #plt.imshow(tf.keras.utils.array_to_img(np.repeat(np.expand_dims(mask_img,-1),3,-1)), cmap = "jet", alpha = 0.2)
+    elif k == ord('r'):#use this if i mess up creating mask
+        layer_1_x = []
+        layer_1_y = []
 
-print(layer_1_x[1:10])
-print(layer_2_x[1:10])
-print(layer_3_x[1:10])
+        layer_2_x = []
+        layer_2_y = []
+
+        layer_3_x = []
+        layer_3_y = []
+        new_img = reserve_img.copy()
+    elif k == ord('s'):
+        cv2.imwrite('E:/3Dimage/Intensity/OCT_Pupillary_Test/corneadata/input/' + 'input_' + str(i) + '.jpg', reserve_img.copy())
+        cv2.imwrite('E:/3Dimage/Intensity/OCT_Pupillary_Test/corneadata/output/' + 'output_' + str(i) + '.jpg', mask_img)
+        print("save data_" + str(i))
+
+
+
